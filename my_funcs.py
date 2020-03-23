00acecc9c0fa8508a498334a5e45ffa2656d0217
Kaiping Ma <kaipingma@Kaipings-MacBook-Air.local>
2020-03-22 18:43:50 -0700

    Initial commit

---
+++ my_funcs.py
@@ -0,0 +1,45 @@
+"""
+>>> func1()
+'func1'
+>>> func2()
+'func2'
+>>> func3()
+'func3'
+"""
+
+
+def func1():
+    """
+    >>> func1()
+    'func1'
+    """
+    # print('in func1...')
+    return 'func1'
+
+
+def func2():
+    """
+    >>> func2()
+    'func2'
+    """
+    # print('in func2...')
+    return 'func2'
+
+
+def func3():
+    """
+    >>> func3()
+    'func3'
+    """
+    # print('in func3...')
+    return 'func3'
+
+
+if __name__ == '__main__':
+    # import doctest
+    # doctest.testmod(verbose=True)
+    print('In main, calling', func1())
+    print('In main, calling', func2())
+    print('In main, calling', func3())
+
+# print(func1(), func2(), func3())
---
+++ my_main.py
@@ -0,0 +1,11 @@
+"""
+>>> my_funcs.func1()
+'func1'
+"""
+import my_funcs
+
+
+if __name__ == '__main__':
+    print('IN MAIN...')
+    print(my_funcs.func1())
+    print('END OF MAIN')
